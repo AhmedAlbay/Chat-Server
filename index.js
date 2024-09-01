434b3554c35f52831f6f066c5abbf2a7a313f69a
AhmedAlbay <ahmed.m.albay@gmail.com>
2024-09-01 18:02:23 +0300

    finish some of chat server

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-const express =require ("express");
-var http =require ("http");
-const cors =require ("cors");
-const exp = require("constants");
-const app= express();
-const port =process.env.PORT || 5000 ;
-var server =http.createServer(app);
-var io = require("socket.io")(server ,{
-    cors:
-    {
-        origin:"*"
-    }
-});
+const express = require("express");
+var http = require("http");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+// Middleware
 app.use(express.json());
-app.use(cors());
-io.on("connection",()=>{
-    console.log("Connected");
+
+var clients = {};
+
+io.on("connection", (socket) => {
+  console.log("Connected");
+  console.log(socket.id, "has joined");
+
+  socket.on("signin", (id) => {
+    console.log(id);
+    clients[id] = socket;
+    console.log(clients);
+  });
+
+  socket.on("message", (msg) => {
+    console.log("Message received:", msg);
+    let targetSocket = clients[msg.targetId];
+    if (targetSocket) {
+      targetSocket.emit("message", msg);
+      console.log(`Message sent to ${msg.targetId}`);
+    }
+  });
 });
-server.listen(port,()=>{
-    console.log("Server started");
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
